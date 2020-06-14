74cdd4e8f35e73d7c0f96204f18123c3893f0019
javyak <javier.yago@gmail.com>
2020-06-14 17:18:52 +0200

    Configuratin for cloudwatch agent

---
+++ S06/jump_init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Install NGINX
+sudo apt-get update
+sudo apt-get --assume-yes update 
+
+# Apply FW rules that allow 22 for inbound conections.
+sudo ufw allow 22/tcp
+sudo ufw --force enable
+
+# SSH server confioguration already included in default image.
+
+# Download and install cloudwatch agent
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/arm64/latest/amazon-cloudwatch-agent.deb
+sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
+# Configure cloudwatch agent with the file in the gir repository
+cd /opt/aws/amazon-cloudwatch-agent/bin 
+sudo wget https://raw.githubusercontent.com/javyak/nw_public_cloud/master/S06/config.json
+# Start the cloudwatch agent
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s
+
+
---
+++ S06/web_init.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Install NGINX
+sudo apt-get update
+sudo apt-get --assume-yes update 
+sudo apt-get --assume-yes install nginx
+
+# Apply FW rules that allow 80, 22 and 443 for inbound conections.
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw allow 22/tcp
+sudo ufw --force enable
+
+# Create a certificate for the SSL service
+sudo openssl req -x509 \
+    -days 365 \
+    -sha256 \
+    -newkey rsa:2048 \
+    -nodes \
+    -keyout /etc/ssl/private/regportal.key \
+    -out /etc/ssl/certs/regportal-cert.pem \
+    -subj "/C=ES/ST=Madrid/L=Madrid/O=Regportal/OU=IoT/CN=regportal.lan"
+
+# Create the website for the IoT registration portal service
+sudo mkdir /var/www/regportal
+cd /var/www/regportal
+sudo wget https://raw.githubusercontent.com/javyak/nw_public_cloud/master/S04/index.html
+# Complete the Html configuration by appending the 
+cd /var/www/regportal
+sudo bash -c "sed -i s/S3_BUCKET/$1/ index.html"
+sudo bash -c "sed -i s/PRIVATE_IP_ADDR/$2/ index.html"
+# Configure the web server settings
+cd /etc/nginx/sites-available/
+sudo wget https://raw.githubusercontent.com/javyak/nw_public_cloud/master/S04/regportal_ssl
+sudo ln -s /etc/nginx/sites-available/regportal_ssl /etc/nginx/sites-enabled/regportal
+sudo systemctl restart nginx
+
+
