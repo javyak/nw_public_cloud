4325bf07f16b2320fb022bbcb2d1674624dd746e
javyak <javier.yago@gmail.com>
2020-06-29 23:56:59 +0200

    Load balancers file updated

---
+++ S06/loadbalancers.tf
@@ -0,0 +1,72 @@
+# Application Load Balancer
+
+resource "aws_lb" "iot-web-alb" {
+  name               = "iot-web-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.web_alb.id]
+  subnets            = [aws_subnet.public1.id,aws_subnet.public2.id]
+}
+
+
+resource "aws_lb_target_group" "iot-http-tg" {
+  name     = "iot-http-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.iot_service.id
+}
+
+resource "aws_lb_target_group" "iot-ssl-tg" {
+  name     = "iot-ssl-tg"
+  port     = 443
+  protocol = "HTTPS"
+  vpc_id   = aws_vpc.iot_service.id
+}
+
+resource "aws_lb_target_group_attachment" "iot-http-att1" {
+  target_group_arn = aws_lb_target_group.iot-http-tg.arn
+  target_id        = aws_instance.web_server1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "iot-ssl-att1" {
+  target_group_arn = aws_lb_target_group.iot-ssl-tg.arn
+  target_id        = aws_instance.web_server1.id
+  port             = 443
+}
+
+resource "aws_lb_target_group_attachment" "iot-http-att2" {
+  target_group_arn = aws_lb_target_group.iot-http-tg.arn
+  target_id        = aws_instance.web_server2.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "iot-ssl-att2" {
+  target_group_arn = aws_lb_target_group.iot-ssl-tg.arn
+  target_id        = aws_instance.web_server2.id
+  port             = 443
+}
+
+resource "aws_lb_listener" "iot-web-listener-80" {
+  load_balancer_arn = aws_lb.iot-web-alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+ 
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.iot-http-tg.arn
+  }
+}
+
+resource "aws_lb_listener" "iot-web-listener-443" {
+  load_balancer_arn = aws_lb.iot-web-alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:acm:eu-west-1:151803822585:certificate/6eaada10-2f24-4f77-8b14-c914678f7b78"
+# Certificated alreeady created and uploaded to AWS ACM
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.iot-ssl-tg.arn
+  }
+}
